c93a2f256378980afd1414c8cd7afb44b524ecc0
deeniedoank <deeniedoank@gmail.com>
2017-03-25 12:39:45 +0700

    Update debian8x64.sh

--- debian8x64.sh
+++ debian8x64.sh
@@ -129,10 +129,10 @@ service fail2ban restart
 
 # install webmin
 cd
-wget http://prdownloads.sourceforge.net/webadmin/webmin_1.831_all.deb
-dpkg --install webmin_1.831_all.deb
+wget http://prdownloads.sourceforge.net/webadmin/webmin_1.820_all.deb
+dpkg --install webmin_1.820_all.deb
 apt-get -y -f install
-rm /root/webmin_1.831_all.deb
+rm /root/webmin_1.820_all.deb
 sed -i 's/ssl=1/ssl=0/g' /etc/webmin/miniserv.conf
 service webmin restart
 
