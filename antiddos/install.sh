75120f76bbc6b4ab2daca14ea350858b5693059f
deeniedoank <deeniedoank@gmail.com>
2017-03-30 09:18:00 +0000

    Update install.sh

--- antiddos/install.sh
+++ antiddos/install.sh
@@ -25,5 +25,5 @@ echo '.....done'
 echo; echo 'Installation has completed.'
 echo 'Config file is at /usr/local/ddos/ddos.conf'
 echo 'Please send in your comments and/or suggestions to zaf@vsnl.com'
-echo
-cat /usr/local/ddos/LICENSE | less
+
+exit
