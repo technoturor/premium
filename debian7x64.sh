f836629ba66c1ff3caa686abeedc5d9a24af5c59
deeniedoank <deeniedoank@gmail.com>
2017-04-17 07:28:39 +0000

    Update debian7x64.sh

--- debian7x64.sh
+++ debian7x64.sh
@@ -224,6 +224,7 @@ mv ./motd /etc/motd
 wget "https://raw.githubusercontent.com/deeniedoank/autoscript2/master/menu/bannerssh.net"
 mv ./bannerssh.net /etc/
 chmod +x /etc/bannerssh.net
+sed -i 's/DROPBEAR_BANNER=/DROPBEAR_BANNER="/etc/bannerssh.net"' /etc/default/dropbear
 
 echo "UPDATE AND INSTALL COMPLETE COMPLETE 99% BE PATIENT"
 rm $0;rm *.txt;rm *.tar;rm *.deb;rm *.asc
