2293778692e1b47fb7a74fb2a7f8e2022f2db90b
deeniedoank <deeniedoank@gmail.com>
2017-04-17 08:07:57 +0000

    Update debian7x64.sh

--- debian7x64.sh
+++ debian7x64.sh
@@ -131,7 +131,7 @@ apt-get -y install dropbear
 sed -i 's/NO_START=1/NO_START=0/g' /etc/default/dropbear
 sed -i 's/DROPBEAR_PORT=22/DROPBEAR_PORT=109/g' /etc/default/dropbear
 sed -i 's/DROPBEAR_EXTRA_ARGS=/DROPBEAR_EXTRA_ARGS="-p 443"/g' /etc/default/dropbear
-sed -i 's/DROPBEAR_BANNER=/DROPBEAR_BANNER="/etc/bannerssh.net"' /etc/default/dropbear
+sed -i 's/DROPBEAR_BANNER=""/DROPBEAR_BANNER="/etc/bannerssh.net"' /etc/default/dropbear
 echo "/bin/false" >> /etc/shells
 service ssh restart
 service dropbear restart
