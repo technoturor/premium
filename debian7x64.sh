4a34692d4b14f7f7a9fe52e54dafdb16804b15fc
deeniedoank <deeniedoank@gmail.com>
2017-04-17 10:02:36 +0000

    Update debian7x64.sh

--- debian7x64.sh
+++ debian7x64.sh
@@ -247,7 +247,7 @@ echo "Webmin   : http://$myip:10000/" | lolcat
 echo "Squid3   : 8080,3128" | lolcat
 echo "OpenSSH  : 22, 80" | lolcat
 echo "Dropbear : 443, 109"| lolcat
-echo "OpenVPN  : TCP Port 55 (client config : http://$myip/client.tar)" | lolcat
+echo "OpenVPN  : TCP Port 55 (client config : http://$myip:8090/client.tar)" | lolcat
 echo "Timezone : Asia/Jakarta"| lolcat
 echo "Fail2Ban : [on]"| lolcat
 echo "Power By : Yusuf Ardiansyah"| lolcat
