18533a914958b9339af0319a47c31397862df246
deeniedoank <deeniedoank@gmail.com>
2017-04-17 05:52:14 +0000

    Update debian7x64.sh

--- debian7x64.sh
+++ debian7x64.sh
@@ -127,19 +127,10 @@ service ssh restart
 
 # install dropbear
 apt-get -y install dropbear
-apt-get install zlib1g-dev
-wget https://raw.githubusercontent.com/deeniedoank/autoscript2/master/update_dropbear/dropbear-2016.74.tar.bz2
-bzip2 -cd dropbear-2016.74.tar.bz2 | tar xvf -
-cd dropbear-2016.74
-./configure
-make && make install
-mv /usr/sbin/dropbear /usr/sbin/dropbear.old
-ln /usr/local/sbin/dropbear /usr/sbin/dropbear
 sed -i 's/NO_START=1/NO_START=0/g' /etc/default/dropbear
 sed -i 's/DROPBEAR_PORT=22/DROPBEAR_PORT=109/g' /etc/default/dropbear
 sed -i 's/DROPBEAR_EXTRA_ARGS=/DROPBEAR_EXTRA_ARGS="-p 443"/g' /etc/default/dropbear
 echo "/bin/false" >> /etc/shells
-cd && rm -rf dropbear-2016.74 && rm -rf dropbear-2016.74.tar.bz2
 service ssh restart
 service dropbear restart
 
