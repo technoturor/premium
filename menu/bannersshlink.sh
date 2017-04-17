5fb4103ce8715f85ca0cdca17a4a058593ab3156
deeniedoank <deeniedoank@gmail.com>
2017-04-17 11:45:43 +0000

    Update bannersshlink.sh

--- menu/bannersshlink.sh
+++ menu/bannersshlink.sh
@@ -1,6 +1,5 @@
-#replace sshd_config
-#sed -i `s/#Banner/Banner` /etc/ssh/sshd_config
-sed -i 's/issue.net/\nBanner bannerssh' /etc/ssh/sshd_config
+sed -i 's/#Banner/Banner' etc/ssh/sshd_config
+sed -i 's/issue.net/bannerssh' /etc/ssh/sshd_config
 
 #replace banner dropbear
 sed -i 's/DROPBEAR_BANNER=/DROPBEAR_BANNER="bannerssh"/g' /etc/default/dropbear
