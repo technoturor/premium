180b5716a5423641bd4f60b050f3af1515304cd7
deeniedoank <deeniedoank@gmail.com>
2017-04-19 23:49:31 +0000

    Update debian7x64.sh

--- debian7x64.sh
+++ debian7x64.sh
@@ -126,6 +126,8 @@ sed -i 's/Port 22/Port  22/g' /etc/ssh/sshd_config
 service ssh restart
 
 # install dropbear
+
+rm /etc/default/dropbear
 apt-get -y install dropbear
 sed -i 's/NO_START=1/NO_START=0/g' /etc/default/dropbear
 sed -i 's/DROPBEAR_PORT=22/DROPBEAR_PORT=109/g' /etc/default/dropbear
