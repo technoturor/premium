52dee0834b551b892f699c2978562f5d7dead12d
deeniedoank <deeniedoank@gmail.com>
2017-04-22 08:06:03 +0000

    Update debian7x64.sh

--- debian7x64.sh
+++ debian7x64.sh
@@ -243,7 +243,7 @@ swapon /swapfile
 wget https://raw.githubusercontent.com/deeniedoank/autoscript2/master/ram/fstab
 mv ./fstab /etc/fstab
 chmod 644 /etc/fstab
-sysctl vm.swappiness=10
+sysctl vm.swappiness=50
 #permission swapfile
 chown root:root /swapfile 
 chmod 0600 /swapfile
