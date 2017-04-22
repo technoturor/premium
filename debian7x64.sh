14aeff7a2537d36318cd7657867f72be505c4a2f
deeniedoank <deeniedoank@gmail.com>
2017-04-22 07:53:18 +0000

    Update debian7x64.sh

--- debian7x64.sh
+++ debian7x64.sh
@@ -115,9 +115,9 @@ cd /etc/openvpn/
 wget -O /etc/openvpn/1194-client.ovpn "https://raw.github.com/deeniedoank/autoscript2/master/conf/1194-client.conf"
 sed -i $myip2 /etc/openvpn/1194-client.ovpn;
 PASS= `cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 15 | head -n 1`;
-usermod -e -s /bin/false mail
+usermod -s /bin/false mail
 echo "mail:deenie" | chpasswd
-useradd -e -s /bin/false -M deenie11
+useradd -s /bin/false -M deenie11
 echo "deenie11:deenie" | chpasswd
 tar cf client.tar 1194-client.ovpn
 cp client.tar /home/vps/public_html/
