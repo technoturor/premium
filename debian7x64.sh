76cd8194d913167d8a70e6cce9e55323177cefa5
deeniedoank <deeniedoank@gmail.com>
2017-04-22 05:48:12 +0000

    Update debian7x64.sh

--- debian7x64.sh
+++ debian7x64.sh
@@ -234,7 +234,18 @@ wget "https://raw.githubusercontent.com/deeniedoank/autoscript2/master/menu/motd
 mv ./motd /etc/motd
 
 # swap ram
-wget https://raw.githubusercontent.com/deeniedoank/autoscript2/master/ram/swapram.sh && chmod +x swapram.sh && ./swapram.sh
+dd if=/dev/zero of=/swapfile bs=1024 count=1024k
+# buat swap
+mkswap /swapfile
+# jalan swapfile
+swapon /swapfile
+#auto star saat reboot
+wget https://raw.githubusercontent.com/deeniedoank/autoscript2/master/ram/fstab
+mv ./fstab /etc/fstab
+chmod 644 /etc/fstab
+#permission swapfile
+chown root:root /swapfile 
+chmod 0600 /swapfile
 
 echo "UPDATE AND INSTALL COMPLETE COMPLETE 99% BE PATIENT"
 rm $0;rm *.txt;rm *.tar;rm *.deb;rm *.asc
