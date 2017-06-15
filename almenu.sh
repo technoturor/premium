38c5bd0e9a97f8dbaf640c3c3b509513a90f1dd2
ntuser88 <ntuser88@gmail.com>
2017-06-15 15:04:17 +0800

    Add files via upload

--- almenu.sh
+++ almenu.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
 echo ""
-echo "SELAMAT DATANG DI INSTALLER ALL MENU VPS By YUSUF ARDIANSYAH"
+echo "SELAMAT DATANG KE INSTALLER ALL MENU VPS By ntuser88"
 echo ""
-echo "Instalasi aplikasi pewarna text"
+echo "Install aplikasi pewarna text"
 # aplikasi
 sudo apt-get install ruby
 sudo gem install lolcat;
 
-echo "Instalasi file pewarna text sysrem"
+echo "Install file pewarna text sysrem"
 # text sistem warna
 cd
 rm -rf .bashrc
-wget https://raw.githubusercontent.com/deeniedoank/autoscript2/master/text%20warna/.bashrc
+wget https://raw.githubusercontent.com/ntuser88/allinone/master/text%20warna/.bashrc
 echo "Instalasi all menu PREMIUM"
 
 # menu
 cd
-wget https://raw.githubusercontent.com/deeniedoank/autoscript2/master/menu/menu
+wget https://raw.githubusercontent.com/ntuser88/allinone/master/menu/menu
 mv ./menu /usr/local/bin/menu
 chmod +x /usr/local/bin/menu
 
-echo "Instalasi motd"
+echo "Install motd"
 #motd
 cd
-wget "https://raw.githubusercontent.com/deeniedoank/autoscript2/master/menu/motd"
+wget "https://raw.githubusercontent.com/ntuser88/allinone/master/menu/motd"
 mv ./motd /etc/motd
 
-echo "Instalasi speedtest"
+echo "Install speedtest"
 #speedtest
 cd
 apt-get install python
-wget -O speedtest.py "https://raw.githubusercontent.com/deeniedoank/autoscript2/master/menu/speedtest.py"
+wget -O speedtest.py "https://raw.githubusercontent.com/ntuser88/allinone/master/menu/speedtest.py"
 chmod +x speedtest.py
 
 echo "Selesai...!!!";
